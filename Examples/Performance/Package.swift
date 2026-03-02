b7ca7781c787b75329a30560b6568c148d151686
Pavlo Ivashkov <pavels.code@gmail.com>
2026-03-02 21:01:05 +0200

    update

--- Examples/Performance/Package.swift
+++ Examples/Performance/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v15),
     ],
     dependencies: [
-        .package(path: "../../")
+        .package(url: "https://github.com/paiv/swift-pcg-random.git", from: "2.0.0")
     ],
     targets: [
         .executableTarget(
